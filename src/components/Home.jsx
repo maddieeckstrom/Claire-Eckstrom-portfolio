1ffc249d01efa4c9b199f22dd2324f8ef4412315
Maddie <maddieeckstrom@gmail.com>
2024-02-03 09:13:06 -1000

    added component home and basic home skeleton

---
+++ src/components/Home.jsx
@@ -0,0 +1,22 @@
+import './App.css'
+
+export default function Home() {
+  return (
+    <section>
+      <div>
+        <h1>Claire Eckstrom</h1>
+        <h1>Consulting</h1>
+      </div>
+      <div>
+        <link to>Fashion, Luxury, &  Lifestyle Brands</link>
+      </div>
+      <div>
+        <link to>Bars & Restaurants</link>
+      </div>
+      <div>
+        <link to>Other creatives</link>
+      </div>
+    </section>
+  )
+}
+
