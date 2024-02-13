8ebdfbc839372170462ed258e77423d80e7edf89
Maddie <maddieeckstrom@gmail.com>
2024-02-13 11:21:38 -1000

    added motion frames and began adding animation to header on home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import '../App.css'
+import { motion } from "framer-motion";
 
 export default function Home({currentPage, handlePageChange}) {
 
   const [showContent, setShowContent] = useState(false);
-  const [isHeaderVisible, setIsHeaderVisible] = useState(true);
+  const [isVisible, setIsVisible] = useState(true)
+  // const [isHeaderVisible, setIsHeaderVisible] = useState(true);
 
   useEffect(() => {
     const timeout = setTimeout(() => {
@@ -14,30 +16,51 @@ export default function Home({currentPage, handlePageChange}) {
     return () => clearTimeout(timeout);
   }, []);
 
-  useEffect(() => {
+  const handleAnimationComplete = () => {
+    setIsVisible(false);
+  };
+
+  // useEffect(() => {
     // Set interval to toggle header visibility every 1/2 second
-    const intervalId = setInterval(() => {
-      setIsHeaderVisible(prevState => !prevState);
-    }, 500);
+    // const intervalId = setInterval(() => {
+    //   setIsHeaderVisible(prevState => !prevState);
+    // }, 500);
 
     // Clear after 2 seconds
-    setTimeout(() => {
-      clearInterval(intervalId);
-      setIsHeaderVisible(true);
-    }, 2000);
+    // setTimeout(() => {
+    //   clearInterval(intervalId);
+    //   setIsHeaderVisible(true);
+    // }, 2000);
 
     // Cleanup function to clear interval
-    return () => clearInterval(intervalId);
-  }, []);
+  //   return () => clearInterval(intervalId);
+  // }, []);
   
   return (
     <section className="section-container">
-      {isHeaderVisible && (
+      {/* {isHeaderVisible && ( */}
         <div className="header">
+          {isVisible && (
+            <div style={{ position: 'relative' }}>
+              <motion.div
+                style={{ position: 'absolute', top: 0, left: 0 }}
+                initial={{ x: 239, y: 0 }}
+                animate={{
+                  x: [239, 350, 500, 350], 
+                  y: [0, 0, 100, 200], 
+                  transition: { duration: 2, repeat: 0, type: 'keyframes' }
+                }}
+                onAnimationComplete={handleAnimationComplete}
+                // path={{ path: "M239 0 Q 500 100, 350 200" }}
+              >
+                <p>¨</p>
+              </motion.div>
+            </div>
+          )}
           <p>CLAİRE ECKSTRÖM</p>
           <p>CONSULTING</p>
         </div>
-      )}
+      {/* )} */}
       <div
         className="home-nav" 
         style={{ display: showContent ? 'grid' : 'none' }}
